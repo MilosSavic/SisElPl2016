77c8eb819bae1e352b3b9f11424545c3c4aa0488
Vladimir Baumgartner <gartn3r@gmail.com>
2016-11-16 21:02:31 +0100

    Initial commit of AcquirerBankWS Spring Boot application

---
+++ AcquirerBankWS/src/main/java/rs/ac/uns/ftn/sep2016/AcquirerBankWsApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn.sep2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquirerBankWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquirerBankWsApplication.class, args);
+	}
+}
