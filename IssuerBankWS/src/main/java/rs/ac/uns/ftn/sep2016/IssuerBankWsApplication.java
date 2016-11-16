aa82f6c47e258c8fff15a8c36449e59a4e583f56
Vladimir Baumgartner <gartn3r@gmail.com>
2016-11-16 21:02:54 +0100

    Initial commit of IssuerBankWS Spring Boot application

---
+++ IssuerBankWS/src/main/java/rs/ac/uns/ftn/sep2016/IssuerBankWsApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn.sep2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuerBankWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuerBankWsApplication.class, args);
+	}
+}
