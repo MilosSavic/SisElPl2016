d6c198b0b4abda82d604a42084e3c45dbfc9474e
Vladimir Baumgartner <gartn3r@gmail.com>
2016-11-16 21:03:21 +0100

    Initial commit of PaymentCardCenterWS Spring Boot application

---
+++ PaymentCardCenterWS/src/main/java/rs/ac/uns/ftn/sep2016/PaymentCardCenterWsApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn.sep2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCardCenterWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentCardCenterWsApplication.class, args);
+	}
+}
