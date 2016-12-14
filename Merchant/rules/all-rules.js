14d3dbefb72ca783899e7402476b9d8f167915f1
MilosSavic <savic.milan@sezampro.rs>
2016-12-15 00:52:23 +0100

    Moving all rules into one file

--- Merchant/rules/all-rules.js
+++ Merchant/rules/all-rules.js
@@ -3,7 +3,8 @@ module.exports.execute = execute;
 
 var i = {body:{amount:50000,region:"585012cb27d27e0b80ad1dff",duration:11,numberOfUsers:3,
 	users:[{age:50,sport:"58411ff9e0a20212f8dd8db4"},{age:77,sport:"5851a3bbd55b031f41487e71"}],
-	houseInsurance:{age:35,size:455,estimatedValue:55900,coveredByInsurance:["585026c95eda23124ca18b7e","5851cf33f478070be41d3dd0","5851cf3ef478070be41d3dd1"]}
+	houseInsurance:{age:35,size:455,estimatedValue:55900,coveredByInsurance:["585026c95eda23124ca18b7e","5851cf33f478070be41d3dd0","5851cf3ef478070be41d3dd1"]},
+	carInsurance: {services:["5850286f5eda23124ca18b82","585028615eda23124ca18b80"]}
 }};
 execute(i, function(result){
 	console.log(result);
@@ -37,6 +38,13 @@ function execute(insurance,result){
 	var ValueFired = flow.getDefined("valueFired");
 	var CoversFired = flow.getDefined("CoversFired");
 
+	//CAR INSURANCE
+
+	var CarInsuranceService = mongoose.model('CarInsuranceService');
+	var CarInsurance = flow.getDefined("carInsurance");
+	var ServicesFired = flow.getDefined("ServicesFired");
+
+
 
 	
 
@@ -56,11 +64,15 @@ function execute(insurance,result){
 	session.assert(new ValueFired(false));
 	session.assert(new CoversFired(false));
 
+	//CAR INSURANCE
+	session.assert(new ServicesFired(false));
+
 	var price = new Price(0);
 	session.assert(price);
 	var regionRisk = -404;
 	var users = [];
 	var covers = [];
+	var services = [];
 	Region.findById(insurance.body.region).exec(function(err,region){
 	    if(err)
 	    {
@@ -90,8 +102,16 @@ function execute(insurance,result){
 	    	//zasto err ne bude definisan?
 	    	if(userSport)
 	     	 users.push({age: user.age,sportRisk: sport.coefficient})
-	     	if(users.length==insurance.body.users.length)
-	      findHouseInsuranceCategories();
+	     	if(users.length==insurance.body.users.length){
+	     	if(insurance.body.houseInsurance)
+	      	findHouseInsuranceCategories();
+	      	else if(insurance.body.carInsurance){
+	      		findCarInsuranceServices();
+	      	}
+	      	else {
+	      		otherStuff();
+	      	}
+	  		}
 	    }
 
 	  });
@@ -111,7 +131,30 @@ function execute(insurance,result){
 	    	//zasto err ne bude definisan?
 	    	if(insurance.body.houseInsurance.coveredByInsurance)
 	      	covers.push(category.riskFactor);
-	      	if(covers.length==insurance.body.houseInsurance.coveredByInsurance.length)
+	      	if(covers.length==insurance.body.houseInsurance.coveredByInsurance.length){
+	      		if(insurance.body.carInsurance)
+	      		findCarInsuranceServices();
+	      		else otherStuff();
+	      	}
+	     
+	    }
+
+	  });
+}
+	}
+	function findCarInsuranceServices(){
+		for(var i=0; i<insurance.body.carInsurance.services.length; i++){
+		CarInsuranceService.findById(insurance.body.carInsurance.services[i]).exec(function(err,category){
+	    if(err)
+	    {
+	      return res.status(400).send({
+	        message: "Error"
+	      });
+	    }else {
+	    	//zasto err ne bude definisan?
+	    	if(insurance.body.carInsurance.services)
+	      	services.push(category.riskFactor);
+	      	if(services.length==insurance.body.carInsurance.services.length)
 	      		otherStuff();
 	     
 	    }
@@ -119,7 +162,6 @@ function execute(insurance,result){
 	  });
 }
 	}
-	
 
 	function otherStuff(){
 		//DEFINISEMO OSNOVNU CENU ZA KORISNIKA
@@ -144,11 +186,20 @@ function execute(insurance,result){
 
 
 	//HOME INSURANCE
+	if(insurance.body.houseInsurance){
 	var age = insurance.body.houseInsurance.age;
 	var size = insurance.body.houseInsurance.size;
 	var value = insurance.body.houseInsurance.estimatedValue; 
 	//session.assert(new Message("goodbye"));
 	session.assert(new HouseInsurance(size,age,value,covers,0));
+	}
+	//da li je ovo ok?
+	else session.assert(new HouseInsurance(0,0,0,[0],0));
+	//CAR INSURANCE
+	if(insurance.body.carInsurance)
+		session.assert(new CarInsurance(services));
+	//da li je ovo ok?
+	else session.assert(new CarInsurance([0]));
 
 	session.match(function(err){
 	    if(err){
